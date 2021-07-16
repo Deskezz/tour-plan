ab6b2f4e4c86c57c7a8fb44308c62afc881c95c8
Deskezz <deskezz228@gmail.com>
2021-07-16 07:12:00 +0400

    create: add reviews section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -10,7 +10,23 @@ const swiper = new Swiper('.swiper-container', {
   
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Keyboard control
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 });
